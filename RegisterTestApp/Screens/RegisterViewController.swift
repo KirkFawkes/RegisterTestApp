92cb4a4abb5f81cdb558c734207ce1ea87c3c8bd
Igor Zubko <igor.zubko@icloud.com>
2016-09-30 11:43:34 +0300

    Renamed default view controller

---
+++ RegisterTestApp/Screens/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  RegisterTestApp
+//
+//  Created by Igor Zubko on 29.09.16.
+//  Copyright © 2016 Igor Zubko. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
