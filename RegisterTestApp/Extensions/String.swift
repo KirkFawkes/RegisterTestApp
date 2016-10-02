d2452f164a18e30f4b18be03b57231c0a03dc943
Igor Zubko <igor.zubko@icloud.com>
2016-10-02 23:51:52 +0300

    Refactored email/password validator

--- RegisterTestApp/Extensions/String.swift
+++ RegisterTestApp/Extensions/String.swift
@@ -8,31 +8,23 @@
 
 import Foundation
 
-class Validator {
+class Validators {
 	// NOTE: Not sure that this good idea to validate email in this way. Usually I just check with /.+@.+/ regex and confirmation email.
 	//       See https://habrahabr.ru/post/280798/ , https://habrahabr.ru/post/175375/ for details
-	static private let emailRegex = "^[a-zA-Z0-9_.+-]{3,}@[a-zA-Z0-9-]{3,}\\.[a-zA-Z0-9-.]{2,}$"
-	static private let emailPredicate = NSPredicate(format: "SELF MATCHES %@", Validator.emailRegex)
-	
-	static private let passwordRegex = "^.{5,}$"
-	static private let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", Validator.passwordRegex)
-	
-	static func isValidEmail(email: String?) -> Bool {
-		return self.emailPredicate.evaluate(with: email ?? "")
-	}
-	
-	static func isValidPassword(password: String?) -> Bool {
-		return self.passwordPredicate.evaluate(with: password ?? "")
-	}
+	static let email    = NSPredicate(format: "SELF MATCHES %@", "^[a-zA-Z0-9_.+-]{3,}@[a-zA-Z0-9-]{3,}\\.[a-zA-Z0-9-.]{2,}$")
+	static let password = NSPredicate(format: "SELF MATCHES %@", "^.{5,}$")
 }
 
+func isValid(_ str: String?, predicate: NSPredicate) -> Bool? {
+	return str.map { value in predicate.evaluate(with: value) }
+}
 
 extension String {
 	func isValidEmail() -> Bool {
-		return Validator.isValidEmail(email: self)
+		return isValid(self, predicate: Validators.email)!
 	}
 	
 	func isValidPassword() -> Bool {
-		return Validator.isValidPassword(password: self)
+		return isValid(self, predicate: Validators.password)!
 	}
 }
